b2c785090cd95ffa8a8eb6daf554e30c997a5c25
Luis Angel Valencia Ramos <luvalencia@sistecredito.com>
2017-06-09 16:57:07 -0500

    Se crea UserControl para Documentos Adicionales, imagenes

--- ConceptosStyles/ConceptosStyles/Views/SolicitudesPrincipal.xaml.cs
+++ ConceptosStyles/ConceptosStyles/Views/SolicitudesPrincipal.xaml.cs
@@ -9,7 +9,74 @@ namespace ConceptosStyles.Views
             InitializeComponent();
 
             cvSolicitudesRefPersonales.BindingContext = BindingContext;
+        }
 
+        private void ShowDatosBasicos(object sender, Xamarin.Forms.TappedEventArgs e)
+        {
+            if( !cvSolicitudesDatosBasicos.IsVisible )
+            {
+                cvSolicitudesDatosBasicos.IsVisible = true;
+
+                cvSolicitudesDatosComplementarios.IsVisible = false;
+                cvSolicitudesRefLaborales.IsVisible = false;
+                cvSolicitudesRefPersonales.IsVisible = false;
+                cvSolicitudesDocAdicionales.IsVisible = false;
+            }
+        }
+
+
+        private void ShowDatosComplementarios(object sender, Xamarin.Forms.TappedEventArgs e)
+        {
+            if (!cvSolicitudesDatosComplementarios.IsVisible)
+            {
+                cvSolicitudesDatosComplementarios.IsVisible = true;
+
+                cvSolicitudesDatosBasicos.IsVisible = false;
+                cvSolicitudesRefLaborales.IsVisible = false;
+                cvSolicitudesRefPersonales.IsVisible = false;
+                cvSolicitudesDocAdicionales.IsVisible = false;
+            }
+        }
+
+
+        private void ShowRefLaborales(object sender, Xamarin.Forms.TappedEventArgs e)
+        {
+            if (!cvSolicitudesRefLaborales.IsVisible)
+            {
+                cvSolicitudesRefLaborales.IsVisible = true;
+
+                cvSolicitudesDatosComplementarios.IsVisible = false;
+                cvSolicitudesDatosBasicos.IsVisible = false;
+                cvSolicitudesRefPersonales.IsVisible = false;
+                cvSolicitudesDocAdicionales.IsVisible = false;
+            }
+        }
+
+
+        private void ShowRefPersonales(object sender, Xamarin.Forms.TappedEventArgs e)
+        {
+            if (!cvSolicitudesRefPersonales.IsVisible)
+            {
+                cvSolicitudesRefPersonales.IsVisible = true;
+
+                cvSolicitudesDatosComplementarios.IsVisible = false;
+                cvSolicitudesRefLaborales.IsVisible = false;
+                cvSolicitudesDatosBasicos.IsVisible = false;
+                cvSolicitudesDocAdicionales.IsVisible = false;
+            }
+        }
+
+        private void ShowDocsAdicionales(object sender, Xamarin.Forms.TappedEventArgs e)
+        {
+            if (!cvSolicitudesDocAdicionales.IsVisible)
+            {
+                cvSolicitudesDocAdicionales.IsVisible = true;
+
+                cvSolicitudesDatosComplementarios.IsVisible = false;
+                cvSolicitudesRefLaborales.IsVisible = false;
+                cvSolicitudesRefPersonales.IsVisible = false;
+                cvSolicitudesDatosBasicos.IsVisible = false;
+            }
         }
     }
 }
