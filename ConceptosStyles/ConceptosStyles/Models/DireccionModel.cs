1b808854fe73150576b5ce7e6fde34c167277c0a
Luis Angel Valencia Ramos <luvalencia@sistecredito.com>
2017-06-20 11:41:32 -0500

    Se agrega archivos ignorados

---
+++ ConceptosStyles/ConceptosStyles/Models/DireccionModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosStyles.Models
+{
+    public class DireccionModel : INotifyPropertyChanged
+    {
+        private string ciudadNombre;
+        private string departamentoNombre;
+        private string barrioNombre;
+
+
+        public string CiudadNombre
+        {
+            get { return ciudadNombre; }
+            set
+            {
+                ciudadNombre = value;
+                OnPropertyChanged("CiudadNombre");
+            }
+        }
+
+        public string DepartamentoNombre
+        {
+            get { return departamentoNombre; }
+            set
+            {
+                departamentoNombre = value;
+                OnPropertyChanged("DepartamentoNombre");
+            }
+        }
+
+        public string BarrioNombre
+        {
+            get { return barrioNombre; }
+            set
+            {
+                barrioNombre = value;
+                OnPropertyChanged("BarrioNombre");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ ConceptosStyles/ConceptosStyles/Models/SimuladorModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosStyles.Models
+{
+    public class SimuladorModel: INotifyPropertyChanged
+    {
+        private string planes;
+        private string frecuencia;
+        private string planesEspeciales;
+
+        public string Planes
+        {
+            get { return planes; }
+            set
+            {
+                planes = value;
+                OnPropertyChanged("Planes");
+            }
+        }
+
+
+        public string Frecuencia
+        {
+            get { return frecuencia; }
+            set
+            {
+                frecuencia = value;
+                OnPropertyChanged("Frecuencia");
+            }
+        }
+
+
+        public string PlanesEspeciales
+        {
+            get { return planesEspeciales; }
+            set
+            {
+                planesEspeciales = value;
+                OnPropertyChanged("PlanesEspeciales");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
